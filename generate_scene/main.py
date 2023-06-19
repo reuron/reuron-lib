338ca7cfe548fb0f57c626724ecb506ece922af1
Gregory Hale <imalsogreg@gmail.com>
2023-06-19 16:19:09 -0700

    wip

--- generate_scene/main.py
+++ generate_scene/main.py
@@ -11,34 +11,26 @@ def fetch_specimens():
     assert(r.status_code == 200)
     return [x['specimen__id'] for x in r.json()["msg"]]
 
+
 def fetch_reconstructions(specimen_id):
     ### Get a list of reconstruction files for a given specimen.
     uri = f'{api_url}/query.json?criteria=model::NeuronReconstruction,rma::criteria,[specimen_id$eq{specimen_id}],rma::include,well_known_files'
     r = requests.get(uri)
     assert(r.status_code == 200)
 
-    def parse_reconstruction(j):
-        print(j)
-        ret = {}
-        print(j.keys())
-        print(j.get("id"))
-        ret.well_known_files = []
-        for f in j["well_known_files"]:
-            fobj = {}
-            fobj.id = f["id"]
-            fobj.path = f["path"]
-            fobj.download_link = f["download_link"]
-            fobj.well_known_file_type_id = f["well_known_file_type_id"]
-            ret.well_known_files.push(fobj)
-        return(ret)
-
-    return([parse_reconstruction(x) for x in r.json()["msg"]])
+    return(r.json()["msg"])
 
 def handle_reconstruction(r):
-    for f in r.well_known_files:
-        if f.well_known_file_type_id == 303941301:
-            print(f.download_link)
-            # print(well_known_file_to_ffg(f.download_link))
+    ### Get the URL of an .swc file and pass it to reuron.io swc converter.
+    for f in r["well_known_files"]:
+        if f["well_known_file_type_id"] == 303941301:
+            link_suffix = f["download_link"]
+            swc_url = f'http:/api.brain-map.org/{link_suffix}'
+
+            print(swc_url)
+            r = requests.get("https://reuron.io/convert-swc", data = swc_url)
+            print(r.text)
+
 
 def well_known_file_to_ffg(well_known_file_url):
     uri = "https://reuron.io/convert_swc"
@@ -47,4 +39,4 @@ def well_known_file_to_ffg(well_known_file_url):
 
 x = fetch_specimens()
 r1 = fetch_reconstructions(x[0])
-handle_reconstruction(r1)
+handle_reconstruction(r1[0])
